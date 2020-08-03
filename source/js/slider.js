80acdc543635df8ddf4879dddc12bae28cec09f1
tomhell27 <53575983+tomhell27@users.noreply.github.com>
2020-08-04 01:24:54 +0300

    Завершает слайдер с отзывами

--- source/js/slider.js
+++ source/js/slider.js
@@ -2,25 +2,45 @@
 
 (function () {
   var feedbacks = document.querySelectorAll('.feedback__item');
-  feedbacks.forEach(function (e) {
-    e.style.display = 'none';
-  });
+  var hideFeedback = function () {
+    feedbacks.forEach(function (e) {
+      e.style.display = 'none';
+    });
+  };
 
+  var number = document.querySelector('.toggle__number--left');
   var index = 0;
 
-  feedbacks[index].style.display = 'block';
+  var changeFeedback = function () {
+    feedbacks[index].style.display = 'block';
+  };
+
+  var changeNumber = function () {
+    number.textContent = (index + 1);
+  };
 
   var left = document.querySelector('.toggle__left');
   var right = document.querySelector('.toggle__right');
 
   left.addEventListener('click', function () {
     index -= 1;
+    if (index <= 0) {
+      index = 0;
+    }
+    hideFeedback();
+    changeFeedback();
+    changeNumber();
   });
 
   right.addEventListener('click', function () {
     index += 1;
+    if (index >= feedbacks.length) {
+      index = 0;
+    }
+    hideFeedback();
+    changeFeedback();
+    changeNumber();
   });
-
 
   /*
   var toggle = document.querySelector('.toggle');
