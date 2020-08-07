c742053cbc54ae1e228b89a6de7bc4254601e204
tomhell27 <53575983+tomhell27@users.noreply.github.com>
2020-08-07 12:27:09 +0300

    Вносит правки

--- build/js/swipe.js
+++ build/js/swipe.js
@@ -1,173 +1,116 @@
 'use strict';
 
 (function () {
-  // настройки по умолчанию
-  var settings = Object.assign({}, {
-    minDist: 60,
-    maxDist: 120,
-    maxTime: 700,
-    minTime: 50
-  }, settings);
+
+  var slider = document.querySelector('.izrael-live');
+  var sliderTrack = slider.querySelector('.slider-track');
+  var slides = slider.querySelectorAll('.slide');
+  var arrows = slider.querySelector('.slider-arrows');
+  var prev = arrows.children[0];
+  var next = arrows.children[1];
+  var slideWidth = slides[0].offsetWidth;
+  var slideIndex = 0;
+  var posInit = 0;
+  var posX1 = 0;
+  var posX2 = 0;
+  var posFinal = 0;
+  var transform = 0;
+  var posThreshold = slideWidth * 0.35;
+  var trfRegExp = /[-0-9.]+(?=px)/;
+  var slide = function () {
+    sliderTrack.style.transition = 'transform .5s';
+    sliderTrack.style.transform = 'translate3d(' + (slideIndex * slideWidth) + 'px, 0px, 0px)';
+
+    // делаем стрелку prev недоступной на первом слайде
+    // и доступной в остальных случаях
+    // делаем стрелку next недоступной на последнем слайде
+    // и доступной в остальных случаях
+    prev.classList.toggle('disabled', slideIndex === 0);
+    next.classList.toggle('disabled', slideIndex === --slides.length);
+  };
+  var getEvent = function () {
+    return event.type.search('touch') !== -1 ? event.touches[0] : event;
+    // p.s. event - аргумент по умолчанию в функции
+  };
 
 
-  var swipe = function (element) {
+  var swipeStart = function () {
+    var evt = getEvent();
 
+    // берем начальную позицию курсора по оси Х
+    posInit = posX1 = evt.clientX;
 
-    // коррекция времени при ошибочных значениях
-    if (settings.maxTime < settings.minTime) {
-      settings.maxTime = settings.minTime + 500;
-    }
-    if (settings.maxTime < 100 || settings.minTime < 50) {
-      settings.maxTime = 700;
-      settings.minTime = 50;
-    }
-    var dir;
-    var swipeType;
-    var dist;
-    var isMouse = false;
-    var isMouseDown = false;
-    var startX = 0;
-    var distX = 0;
-    var startY = 0;
-    var distY = 0;
-    var startTime = 0;
-    var support = {
-      pointer: !!('PointerEvent' in window || ('msPointerEnabled' in window.navigator)),
-      touch: !!(typeof window.orientation !== 'undefined' || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || 'ontouchstart' in window || navigator.msMaxTouchPoints || 'maxTouchPoints' in window.navigator > 1 || 'msMaxTouchPoints' in window.navigator > 1)
-    };
+    // убираем плавный переход, чтобы track двигался за курсором без задержки
+    // т.к. он будет включается в функции slide()
+    sliderTrack.style.transition = '';
 
+    // и сразу начинаем отслеживать другие события на документе
+    document.addEventListener('touchmove', swipeAction);
+    document.addEventListener('touchend', swipeEnd);
+    document.addEventListener('mousemove', swipeAction);
+    document.addEventListener('mouseup', swipeEnd);
+  };
+  var swipeAction = function () {
+    var evt = getEvent();
+    // для более красивой записи возьмем в переменную текущее свойство transform
+    var style = sliderTrack.style.transform;
+    // считываем трансформацию с помощью регулярного выражения и сразу превращаем в число
+    transform = +style.match(trfRegExp)[0];
 
-    var getSupportedEvents = function () {
-      switch (true) {
-        case support.pointer:
-          events = {
-            type: 'pointer',
-            start: 'PointerDown',
-            move: 'PointerMove',
-            end: 'PointerUp',
-            cancel: 'PointerCancel',
-            leave: 'PointerLeave'
-          };
-          // добавление префиксов для IE10
-          var ie10 = (window.navigator.msPointerEnabled && Function('/*@cc_on return document.documentMode===10@*/')());
-          for (var value in events) {
-            if (value === 'type') {
-              continue;
-            }
-            events[value] = (ie10) ? 'MS' + events[value] : events[value].toLowerCase();
-          }
-          break;
-        case support.touch:
-          events = {
-            type: 'touch',
-            start: 'touchstart',
-            move: 'touchmove',
-            end: 'touchend',
-            cancel: 'touchcancel'
-          };
-          break;
-        default:
-          events = {
-            type: 'mouse',
-            start: 'mousedown',
-            move: 'mousemove',
-            end: 'mouseup',
-            leave: 'mouseleave'
-          };
-          break;
+    posX2 = posX1 - evt.clientX;
+    posX1 = evt.clientX;
+
+    sliderTrack.style.transform = 'translate3d(' + (transform - posX2) + 'px, 0px, 0px)';
+    // можно было бы использовать метод строк .replace():
+    // sliderTrack.style.transform = style.replace(trfRegExp, match => match - posX2);
+    // но в дальнейшем нам нужна будет текущая трансформация в переменной
+  };
+
+  var swipeEnd = function () {
+    // финальная позиция курсора
+    posFinal = posInit - posX1;
+
+    document.removeEventListener('touchmove', swipeAction);
+    document.removeEventListener('mousemove', swipeAction);
+    document.removeEventListener('touchend', swipeEnd);
+    document.removeEventListener('mouseup', swipeEnd);
+
+    // убираем знак минус и сравниваем с порогом сдвига слайда
+    if (Math.abs(posFinal) > posThreshold) {
+      // если мы тянули вправо, то уменьшаем номер текущего слайда
+      if (posInit < posX1) {
+        slideIndex -= 1;
+        // если мы тянули влево, то увеличиваем номер текущего слайда
+      } else if (posInit > posX1) {
+        slideIndex += 1;
       }
-      return events;
-    };
-
-
-    var eventsUnify = function (e) {
-      return e.changedTouches ? e.changedTouches[0] : e;
-    };
-
-    var checkStart = function (e) {
-      var event = eventsUnify(e);
-      if (support.touch && typeof e.touches !== 'undefined' && e.touches.length !== 1) {
-        return;
-      }
-      dir = 'none';
-      swipeType = 'none';
-      dist = 0;
-      startX = event.pageX;
-      startY = event.pageY;
-      startTime = new Date().getTime();
-      if (isMouse) {
-        isMouseDown = true;
-      } // поддержка мыши
-      e.preventDefault();
-    };
-
-
-    var checkMove = function (e) {
-      if (isMouse && !isMouseDown) {
-        return;
-      } // выход из функции, если мышь перестала быть активна во время движения
-      var event = eventsUnify(e);
-      distX = event.pageX - startX;
-      distY = event.pageY - startY;
-      if (Math.abs(distX) > Math.abs(distY)) {
-        dir = (distX < 0) ? 'left' : 'right';
-      } else {
-        dir = (distY < 0) ? 'up' : 'down';
-      }
-      e.preventDefault();
-    };
-
-    var checkEnd = function (e) {
-      if (isMouse && !isMouseDown) { // выход из функции и сброс проверки нажатия мыши
-        return;
-      }
-      var endTime = new Date().getTime();
-      var time = endTime - startTime;
-      if (time >= settings.minTime && time <= settings.maxTime) { // проверка времени жеста
-        if (Math.abs(distX) >= settings.minDist && Math.abs(distY) <= settings.maxDist) {
-          swipeType = dir; // опредление типа свайпа как "left" или "right"
-        } else if (Math.abs(distY) >= settings.minDist && Math.abs(distX) <= settings.maxDist) {
-          swipeType = dir; // опредление типа свайпа как "top" или "down"
-        }
-      }
-      dist = (dir === 'left' || dir === 'right') ? Math.abs(distX) : Math.abs(distY); // опредление пройденной указателем дистанции
-
-      // генерация кастомного события swipe
-      if (swipeType !== 'none' && dist >= settings.minDist) {
-        var swipeEvent = new CustomEvent('swipe', {
-          bubbles: true,
-          cancelable: true,
-          detail: {
-            full: e, // полное событие Event
-            dir: swipeType, // направление свайпа
-            dist: dist, // дистанция свайпа
-            time: time // время, потраченное на свайп
-          }
-        });
-        element.dispatchEvent(swipeEvent);
-      }
-      e.preventDefault();
-    };
-
-    // добавление поддерживаемых событий
-    var events = getSupportedEvents();
-
-    // проверка наличия мыши
-    if ((support.pointer && !support.touch) || events.type === 'mouse') {
-      isMouse = true;
     }
 
-    // добавление обработчиков на элемент
-    element.addEventListener(events.start, checkStart);
-    element.addEventListener(events.move, checkMove);
-    element.addEventListener(events.end, checkEnd);
+    // если курсор двигался, то запускаем функцию переключения слайдов
+    if (posInit !== posX1) {
+      slide();
+    }
 
   };
 
-  // элемент
-  var myBlock = document.querySelector('.izrael-live__advantage');
+  arrows.addEventListener('click', function () {
+    var target = event.target;
 
-  // вызов функции swipe с предварительными настройками
-  swipe(myBlock);
+    if (target === next) {
+      slideIndex++;
+    } else if (target === prev) {
+      slideIndex--;
+    } else {
+      return;
+    }
+
+    slide();
+  });
+
+  sliderTrack.style.transform = 'translate3d(0px, 0px, 0px)';
+
+  slider.addEventListener('touchstart', swipeStart);
+  slider.addEventListener('mousedown', swipeStart);
+
 
 })();
--- source/js/swipe.js
+++ source/js/swipe.js
@@ -2,169 +2,96 @@
 
 (function () {
 
-  var settings = Object.assign({}, {
-    minDist: 30,
-    maxDist: 120,
-    maxTime: 700,
-    minTime: 50
-  }, settings);
+  var slider = document.querySelector('.izrael-live');
+  var sliderTrack = slider.querySelector('.slider-track');
+  var slides = slider.querySelectorAll('.slide');
+  var arrows = slider.querySelector('.slider-arrows');
+  var prev = arrows.children[0];
+  var next = arrows.children[1];
+  var slideWidth = slides[0].offsetWidth;
+  var slideIndex = 0;
+  var posInit = 0;
+  var posX1 = 0;
+  var posX2 = 0;
+  var posFinal = 0;
+  var transform = 0;
+  var posThreshold = slideWidth * 0.35;
+  var trfRegExp = /[-0-9.]+(?=px)/;
+  var slide = function () {
+    sliderTrack.style.transition = 'transform .5s';
+    sliderTrack.style.transform = 'translate3d(' + (slideIndex * slideWidth) + 'px, 0px, 0px)';
 
-
-  var swipe = function (element) {
-
-    if (settings.maxTime < settings.minTime) {
-      settings.maxTime = settings.minTime + 500;
-    }
-    if (settings.maxTime < 100 || settings.minTime < 50) {
-      settings.maxTime = 700;
-      settings.minTime = 50;
-    }
-    var dir;
-    var swipeType;
-    var dist;
-    var isMouse = false;
-    var isMouseDown = false;
-    var startX = 0;
-    var distX = 0;
-    var startY = 0;
-    var distY = 0;
-    var startTime = 0;
-    var support = {
-      pointer: !!('PointerEvent' in window || ('msPointerEnabled' in window.navigator)),
-      touch: !!(typeof window.orientation !== 'undefined' || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || 'ontouchstart' in window || navigator.msMaxTouchPoints || 'maxTouchPoints' in window.navigator > 1 || 'msMaxTouchPoints' in window.navigator > 1)
-    };
-
-
-    var getSupportedEvents = function () {
-      switch (true) {
-        case support.pointer:
-          events = {
-            type: 'pointer',
-            start: 'PointerDown',
-            move: 'PointerMove',
-            end: 'PointerUp',
-            cancel: 'PointerCancel',
-            leave: 'PointerLeave'
-          };
-          var ie10 = (window.navigator.msPointerEnabled)();
-          for (var value in events) {
-            if (value === 'type') {
-              continue;
-            }
-            events[value] = (ie10) ? 'MS' + events[value] : events[value].toLowerCase();
-          }
-          break;
-        case support.touch:
-          events = {
-            type: 'touch',
-            start: 'touchstart',
-            move: 'touchmove',
-            end: 'touchend',
-            cancel: 'touchcancel'
-          };
-          break;
-        default:
-          events = {
-            type: 'mouse',
-            start: 'mousedown',
-            move: 'mousemove',
-            end: 'mouseup',
-            leave: 'mouseleave'
-          };
-          break;
-      }
-      return events;
-    };
-
-
-    var eventsUnify = function (e) {
-      return e.changedTouches ? e.changedTouches[0] : e;
-    };
-
-    var checkStart = function (e) {
-      var event = eventsUnify(e);
-      if (support.touch && typeof e.touches !== 'undefined' && e.touches.length !== 1) {
-        return;
-      }
-      dir = 'none';
-      swipeType = 'none';
-      dist = 0;
-      startX = event.pageX;
-      startY = event.pageY;
-      startTime = new Date().getTime();
-      if (isMouse) {
-        isMouseDown = true;
-      }
-      e.preventDefault();
-    };
-
-
-    var checkMove = function (e) {
-      if (isMouse && !isMouseDown) {
-        return;
-      }
-      var event = eventsUnify(e);
-      distX = event.pageX - startX;
-      distY = event.pageY - startY;
-      if (Math.abs(distX) > Math.abs(distY)) {
-        dir = (distX < 0) ? 'left' : 'right';
-      } else {
-        dir = (distY < 0) ? 'up' : 'down';
-      }
-      e.preventDefault();
-    };
-
-    var checkEnd = function (e) {
-      if (isMouse && !isMouseDown) {
-        return;
-      }
-      var endTime = new Date().getTime();
-      var time = endTime - startTime;
-      if (time >= settings.minTime && time <= settings.maxTime) {
-        if (Math.abs(distX) >= settings.minDist && Math.abs(distY) <= settings.maxDist) {
-          swipeType = dir;
-        } else if (Math.abs(distY) >= settings.minDist && Math.abs(distX) <= settings.maxDist) {
-          swipeType = dir;
-        }
-      }
-      dist = (dir === 'left' || dir === 'right') ? Math.abs(distX) : Math.abs(distY);
-
-
-      if (swipeType !== 'none' && dist >= settings.minDist) {
-        var swipeEvent = new CustomEvent('swipe', {
-          bubbles: true,
-          cancelable: true,
-          detail: {
-            full: e,
-            dir: swipeType,
-            dist: dist,
-            time: time
-          }
-        });
-        element.dispatchEvent(swipeEvent);
-      }
-      e.preventDefault();
-    };
-
-
-    var events = getSupportedEvents();
-
-
-    if ((support.pointer && !support.touch) || events.type === 'mouse') {
-      isMouse = true;
-    }
-
-
-    element.addEventListener(events.start, checkStart);
-    element.addEventListener(events.move, checkMove);
-    element.addEventListener(events.end, checkEnd);
-
+    prev.classList.toggle('disabled', slideIndex === 0);
+    next.classList.toggle('disabled', slideIndex === --slides.length);
+  };
+  var getEvent = function () {
+    return event.type.search('touch') !== -1 ? event.touches[0] : event;
   };
 
 
-  var myBlock = document.querySelector('.izrael-live__advantage');
+  var swipeStart = function () {
+    var evt = getEvent();
+    posInit = posX1 = evt.clientX;
+    sliderTrack.style.transition = '';
+    document.addEventListener('touchmove', swipeAction);
+    document.addEventListener('touchend', swipeEnd);
+    document.addEventListener('mousemove', swipeAction);
+    document.addEventListener('mouseup', swipeEnd);
+  };
+  var swipeAction = function () {
+    var evt = getEvent();
 
+    var style = sliderTrack.style.transform;
 
-  swipe(myBlock);
+    transform = +style.match(trfRegExp)[0];
+
+    posX2 = posX1 - evt.clientX;
+    posX1 = evt.clientX;
+
+    sliderTrack.style.transform = 'translate3d(' + (transform - posX2) + 'px, 0px, 0px)';
+  };
+
+  var swipeEnd = function () {
+    posFinal = posInit - posX1;
+
+    document.removeEventListener('touchmove', swipeAction);
+    document.removeEventListener('mousemove', swipeAction);
+    document.removeEventListener('touchend', swipeEnd);
+    document.removeEventListener('mouseup', swipeEnd);
+
+    if (Math.abs(posFinal) > posThreshold) {
+      if (posInit < posX1) {
+        slideIndex -= 1;
+
+      } else if (posInit > posX1) {
+        slideIndex += 1;
+      }
+    }
+
+    if (posInit !== posX1) {
+      slide();
+    }
+
+  };
+
+  arrows.addEventListener('click', function () {
+    var target = event.target;
+
+    if (target === next) {
+      slideIndex++;
+    } else if (target === prev) {
+      slideIndex--;
+    } else {
+      return;
+    }
+
+    slide();
+  });
+
+  sliderTrack.style.transform = 'translate3d(0px, 0px, 0px)';
+
+  slider.addEventListener('touchstart', swipeStart);
+  slider.addEventListener('mousedown', swipeStart);
+
 
 })();
