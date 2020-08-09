d05759c1d8f80d74eb7981c9d355a7f3b3b2bbfa
tomhell27 <53575983+tomhell27@users.noreply.github.com>
2020-08-09 21:52:53 +0300

    Вносит правки

--- build/js/swipe.js
+++ build/js/swipe.js
@@ -2,83 +2,91 @@
 
 (function () {
   var slider = document.querySelector('.izrael-live');
+  var programs = document.querySelector('.programs__anchors-container');
   var sliderTrack = slider.querySelector('.slider-track');
+  var programsTrack = document.querySelector('.programs__anchors');
+  var programsWidth = 300;
   var slideWidth = 300;
   var slideIndex = 0;
   var posInit = 0;
   var posX1 = 0;
   var posX2 = 0;
   var posFinal = 0;
-  var posThreshold = slideWidth * 0.35;
-  var trfRegExp = /[-0-9.]+(?=px)/;
-  var slide = function () {
-    sliderTrack.style.transition = 'transform 0.5s';
-    var newInd = slideIndex * slideWidth;
 
-    sliderTrack.style.transform = 'translate3d(' + (newInd) + 'px, 0px, 0px)';
-  };
-  var getEvent = function () {
-    return event.type.search('touch') !== -1 ? event.touches[0] : event;
-  };
+  var onSlide = function (swipe, section, width, maxwidth) {
+    var posThreshold = width * 0.35;
+    var trfRegExp = /[-0-9.]+(?=px)/;
+    var slide = function () {
+      section.style.transition = 'transform 0.5s';
+      var newInd = slideIndex * width;
+
+      section.style.transform = 'translate3d(' + (newInd) + 'px, 0px, 0px)';
+    };
+    var getEvent = function () {
+      return event.type.search('touch') !== -1 ? event.touches[0] : event;
+    };
 
 
-  var swipeStart = function () {
-    var evt = getEvent();
-    posInit = posX1 = evt.clientX;
-    sliderTrack.style.transition = '';
-    document.addEventListener('touchmove', swipeAction);
-    document.addEventListener('touchend', swipeEnd);
-    document.addEventListener('mousemove', swipeAction);
-    document.addEventListener('mouseup', swipeEnd);
-  };
+    var swipeStart = function () {
+      var evt = getEvent();
+      posInit = posX1 = evt.clientX;
+      section.style.transition = '';
+      document.addEventListener('touchmove', swipeAction);
+      document.addEventListener('touchend', swipeEnd);
+      document.addEventListener('mousemove', swipeAction);
+      document.addEventListener('mouseup', swipeEnd);
+    };
 
-  var swipeAction = function () {
-    var evt = getEvent();
+    var swipeAction = function () {
+      var evt = getEvent();
 
-    var style = sliderTrack.style.transform;
+      var style = section.style.transform;
 
-    var transform = style.match(trfRegExp)[0];
+      var transform = style.match(trfRegExp)[0];
 
-    posX2 = posX1 - evt.clientX;
-    var newTransform = transform - posX2;
-    if (newTransform > 0 || newTransform <= -1200) {
-      sliderTrack.style.transform = 'translate3d(0px, 0px, 0px)';
-      return;
-    } else {
-      posX1 = evt.clientX;
+      posX2 = posX1 - evt.clientX;
+      var newTransform = transform - posX2;
+      if (newTransform > 0 || newTransform <= maxwidth) {
+        section.style.transform = 'translate3d(0px, 0px, 0px)';
+        return;
+      } else {
+        posX1 = evt.clientX;
 
-      sliderTrack.style.transform = 'translate3d(' + newTransform + 'px, 0px, 0px)';
-    }
-
-  };
-
-  var swipeEnd = function () {
-    posFinal = posInit - posX1;
-
-    document.removeEventListener('touchmove', swipeAction);
-    document.removeEventListener('mousemove', swipeAction);
-    document.removeEventListener('touchend', swipeEnd);
-    document.removeEventListener('mouseup', swipeEnd);
-
-    if (Math.abs(posFinal) > posThreshold) {
-      if (posInit < posX1) {
-        slideIndex += 1;
-
-      } else if (posInit > posX1) {
-        slideIndex -= 1;
+        section.style.transform = 'translate3d(' + newTransform + 'px, 0px, 0px)';
       }
-    }
 
-    if (posInit !== posX1) {
-      slide();
-    }
+    };
+
+    var swipeEnd = function () {
+      posFinal = posInit - posX1;
+
+      document.removeEventListener('touchmove', swipeAction);
+      document.removeEventListener('mousemove', swipeAction);
+      document.removeEventListener('touchend', swipeEnd);
+      document.removeEventListener('mouseup', swipeEnd);
+
+      if (Math.abs(posFinal) > posThreshold) {
+        if (posInit < posX1) {
+          slideIndex += 1;
+
+        } else if (posInit > posX1) {
+          slideIndex -= 1;
+        }
+      }
+
+      if (posInit !== posX1) {
+        slide();
+      }
+
+    };
+
+    section.style.transform = 'translate3d(0px, 0px, 0px)';
+
+    swipe.addEventListener('touchstart', swipeStart);
+    swipe.addEventListener('mousedown', swipeStart);
 
   };
 
-  sliderTrack.style.transform = 'translate3d(0px, 0px, 0px)';
-
-  slider.addEventListener('touchstart', swipeStart);
-  slider.addEventListener('mousedown', swipeStart);
-
-
+  onSlide(slider, sliderTrack, slideWidth, -1200);
+  onSlide(programs, programsTrack, programsWidth, -300);
 })();
